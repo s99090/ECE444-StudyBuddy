b44e9ca9136484018ff1060791bf1eda9d7043c2
Sulagshan <sulagshan@Sulagshans-MacBook-Pro-2.local>
2019-11-01 15:47:39 -0400

    remove files from main folder, need to re-add with postgressSQL database

--- app/controllers/discussion_controller.rb
+++
@@ -1,2 +0,0 @@
-class DiscussionController < ApplicationController
-end
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'welcome/index'
-  resources :courses
-  resources :notes
-  resources :discussions
-  resources :links
-  root 'welcome#index'
-end
