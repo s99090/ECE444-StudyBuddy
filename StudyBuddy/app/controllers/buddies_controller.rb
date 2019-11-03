3b6644acc855b3fed93670c1d36c42ba6b78b1fa
syedrokib <syed.rokib@mail.utoronto.ca>
2019-11-03 00:55:42 -0400

    Create Buddies model, view, and controller

--- StudyBuddy/app/controllers/buddies_controller.rb
+++ StudyBuddy/app/controllers/buddies_controller.rb
@@ -1,2 +1,20 @@
 class BuddiesController < ApplicationController
+    def index
+        @buddies = Buddy.all
+    end
+
+    def new
+        @buddy = Buddy.new
+    end
+
+    def create
+        @buddy = Buddy.new(params.require(:buddy).permit(:id, :username, :service, :description, :hourly_rate))
+ 
+        @buddy.save
+        redirect_to @buddy
+    end
+
+    def show
+        @buddy = Buddy.find(params[:id])
+    end
 end
