f62c93cbf7bc128492d5a8e93932e71855e36511
Sulagshan <sulagshan.raveendrakumar@mail.utoronto.ca>
2019-11-03 20:41:39 -0500

    Create links for notes, external links, discussion page, and template view for each.

--- StudyBuddy/app/controllers/links_controller.rb
+++ StudyBuddy/app/controllers/links_controller.rb
@@ -1,2 +1,7 @@
 class LinksController < ApplicationController
+  def new
+  end
+
+  def index
+  end
 end
--- StudyBuddy/config/routes.rb
+++ StudyBuddy/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'welcome/index'
 
-  resources :groups, :buddies, :courses
+  resources :groups, :buddies, :courses, :notes, :links, :discussions
 
   root 'welcome#index'
 end
