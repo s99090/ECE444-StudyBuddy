23688e1c7432b64c41e04c0bc925042410a8bcaf
anirudh-sampath <anirudh.sampath@mail.utoronto.ca>
2019-11-20 15:03:39 -0500

    Applied simle regexp validation for urls pushed in as part of the links tab

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,6 @@
 class Link < ApplicationRecord
     #belongs_to :course
+    #validates :link_url, url: { allow_nil: true }
+    URL_REGEXP = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+    validates :link_url, format: { with: URL_REGEXP, message: 'You provided invalid URL', :multiline => true}
 end
