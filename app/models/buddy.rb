cf2a21542818b89ae546268b8e30a39848feb302
syedrokib <syed.rokib@mail.utoronto.ca>
2019-11-27 19:45:45 -0500

    Add dollar amount validation for buddies hourly rate

--- app/models/buddy.rb
+++ app/models/buddy.rb
@@ -1,3 +1,7 @@
 class Buddy < ApplicationRecord
   validates :username, uniqueness: true, presence: true
+  DOLLAR_AMOUNT_REGEXP = /^\d{1,6}(\.\d{0,2})?$/
+  validates :hourly_rate, numericality: true, :format => { :with => DOLLAR_AMOUNT_REGEXP,
+                                                           :message => "You provided an invalid dollar amount",
+                                                           :multiline => true}
 end
