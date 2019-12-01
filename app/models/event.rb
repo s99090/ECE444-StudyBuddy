c6b06aace8439502bc16e0c624fb6cb2dfe9e0fb
Zhihao Sheng <zhihao.sheng@mail.utoronto.ca>
2019-12-01 17:16:56 -0500

    add dependent for models;

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
   belongs_to :group
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :delete_all
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   validates :name, presence: true
   # has_many :group_announcement
   # has_many :group_meeting
-  has_many :events
-  has_many :groupchats
+  has_many :events, dependent: :destroy
+  has_many :groupchats, dependent: :destroy
   has_and_belongs_to_many :users
 end
