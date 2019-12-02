18651671afaa135080313d10a98ca5adeb27ef3c
Zhihao Sheng <zhihao.sheng@mail.utoronto.ca>
2019-12-02 00:37:26 -0500

    added validations;

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :group
   has_many :comments, as: :commentable, dependent: :delete_all
+
+  validates :event_type, presence: true
+  validates :name, presence: true
 end
