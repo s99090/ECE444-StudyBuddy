236e489f03fb01d1f2721e9a880dec6ed1d2b782
Zhihao Sheng <zhihao.sheng@mail.utoronto.ca>
2019-12-02 00:55:02 -0500

    allow user to change account without their current password;

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -61,6 +61,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
   #   super(resource)
