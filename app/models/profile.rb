e2acbdb3751060de366c8c234f6b88c6576d12f8
Zhihao Sheng <zhihao.sheng@mail.utoronto.ca>
2019-12-03 02:21:53 -0500

    allow phone number to be blank;

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,6 @@ class Profile < ApplicationRecord
   has_one_attached :avatar
   belongs_to :user
 
-  validates :phone, :numericality => true,
+  validates :phone, :numericality => true, :allow_blank => true,
                     :length => { :minimum => 10, :maximum => 15 }
 end
